54b262b92befa695eb3f565cf540284ec000f5d4
actuated <actuated@users.noreply.github.com>
2020-03-20 15:06:24 -0500

    Updated to v2.0 as a rewrite with new options

--- README.md
+++ README.md
@@ -3,15 +3,14 @@ Shell script to check a file containing NTLM hashes for repeated hashes.
 
 # Usage
 ```
-./find-reused-ntds-hashes.sh [input file]
+./find-reused-ntds-hashes.sh [input file] [options]
 ```
  - **[input file]** is used to specify a list of hashes. The expected format is `user:rid:lmhash:nthash`.
- - Provides a count of:
-   - Total hashes
-   - Unique hashes
-   - Number of hashes reused
-   - Combined total of number of times hashes were reused
-   - The number of times the most-reused hash was found
+ - **--file-list [list]** optionally lets you highlight users in the output based on a target list of users.
+   - Matches are case-insensitive, but must be the complete string (username).
+   - Color output relies on `grep ... --color=always`.
+ - **--preserve-domain** optionally shows the domain name for users in the output (the default is to drop it).
+   - This will not add domains for users that did not have one listed in the input file.
  - Output includes:
-   - **reused-hashes-by-count.txt** lists unique hashes repeated, sorted by the number of times reused, formatted as `count	x	lmhash:nthash`.
-   - **reused-hashes-with-users.txt** lists usernames in columns following each line of **reused-hashes-by-count.txt**.
+   - **reused-hashes-by-count-[datestamp].txt** lists unique hashes repeated, sorted by the number of times reused, formatted as `count	x nthash`.
+   - **reused-hashes-with-users-[datestamp].txt** lists usernames in columns following each line of **reused-hashes-by-count.txt**.
--- find-reused-ntds-hashes.sh
+++ find-reused-ntds-hashes.sh
@@ -1,129 +1,177 @@
 #!/bin/bash
-# find-reused-ntds-hashes.sh (v1.0)
+# find-reused-ntds-hashes.sh (v2.0)
 # v1.0 - 11/04/2017 by Ted R (http://github.com/actuated)
+# v2.0 - 03/20/2020 Rewrite with new options
+dateCreated="11/4/2017"
+dateLastMod="3/20/2020"
 
-varOutCount="reused-hashes-by-count.txt"
-varOutUsers="reused-hashes-with-users.txt"
-varYMDHM=$(date +%F-%H-%M)
-varTempAllHashes="frntdsh-temp-all-$varYMDHM.txt"
-varTempUniqHashes="frntdsh-temp-uniq-$varYMDHM.txt"
-varTempUnsortedCount="frntdsh-temp-count-$varYMDHM.txt"
+dtStamp=$(date +%F-%H-%M)
+fileTemp="temp-frnh-$dtStamp.txt"
+fileOutCount="reused-hashes-by-count-$dtStamp.txt"
+fileOutUsers="reused-hashes-with-users-$dtStamp.txt"
+pathOut="."
+formatUsername="NODOMAIN"
+findMode="N"
+
+function fnUsage {
+  echo
+  echo "Script for checking NTDS dumps/NTLM password hash lists for duplicate password hashes"
+  echo "between user accounts."
+  echo
+  echo "Created $dateCreated, last modified $dateLastMod."
+  echo
+  echo "======================================[ usage ]======================================"
+  echo
+  echo "./find-reused-ntds-hashes.sh [NTDS file] [options]"
+  echo
+  echo "[NTDS file]"
+  echo
+  echo "--out-dir [directory]   Optionally specify an output directory for the two files."
+  echo
+  echo "--find-list [file]      Check usernames against a list and color results using grep"
+  echo "                        output. Case insensitive, but must match the whole string."
+  echo "                        Meant to let you supply a list of admins and highlight them"
+  echo "                        if they appear in the output. Optional."
+  echo
+  echo "--preserve-domain       Optionally display usernames with any domain name they had in"
+  echo "                        the original input file."
+  echo
+  echo "=======================================[ fin ]======================================="
+  echo
+}
 
 echo
 echo "==============[ find-reused-ntds-hashes.sh - Ted R (github: actuated) ]=============="
 
-if [ ! -f "$1" ]; then
-  echo
-  echo "Error: No Input File Specified"
-  echo "./find-reused-ntds-hashes.sh [input file]"
-  echo
-  echo "Looking for: user:rid:lmhash:ntlmhash"
-  echo
-  echo "=======================================[ fin ]======================================="
-  echo
-  exit
-fi
-
-if [ -f "$varOutCount" ]; then
-  echo
-  echo "Error: $varOutCount already exists."
-  echo
-  echo "=======================================[ fin ]======================================="
-  echo
-  exit
-fi
-
-if [ -f "$varOutUsers" ]; then
-  echo
-  echo "Error: $varOutUsers already exists."
-  echo
-  echo "=======================================[ fin ]======================================="
-  echo
-  exit
-fi
-
-if [ -f "$varTempAllHashes" ]; then rm "$varTempAllHashes"; fi
-if [ -f "$varTempUniqHashes" ]; then rm "$varTempUniqHashes"; fi
-if [ -f "$varTempUnsortedCount" ]; then rm "$varTempUnsortedCount"; fi
-
-varCheckForHashes=$(grep .*:*:................................:................................ "$1" | wc -l)
-if [ $varCheckForHashes = 0 ]; then
-  echo
-  echo "Error: $1 doesn't seem to contain any NTLM hashes."
-  echo "Grepping for: .*:*:................................:................................"
-  echo
-  echo "=======================================[ fin ]======================================="
-  echo
-  exit
-else
-  echo
-  echo "Found $varCheckForHashes hashes."
-fi
-
-grep .*:*:................................:................................ "$1" > "$varTempAllHashes"
-awk -F: '{print $3 ":" $4}' "$varTempAllHashes" | sort | uniq > "$varTempUniqHashes"
-
-varCheckForUniques=$(wc -l "$varTempUniqHashes" | awk '{print $1}')
-if [ "$varTempUniqHashes" = "wc" ]; then
-  echo
-  echo "Error: Didn't find any user:rid:lmhash:ntlmhash hashes to check for repeats with."
-  echo
-  echo "=======================================[ fin ]======================================="
-  echo
-  exit
-else
-  echo "Found $varCheckForUniques unique password hashes."
-fi
-
-varHighest=0
-varCountTimes=0
-varCountTotal=0
-while read varLine; do
-  varCountReuse=$(grep "$varLine" "$varTempAllHashes" | wc -l)
-  if [ $varCountReuse -gt 1 ]; then
-    echo -e "$varCountReuse\tx\t$varLine" >> "$varTempUnsortedCount"
-    let varCountTimes=varCountTimes+1
-    let varCountTotal=varCountTotal+varCountReuse
-    if [ $varCountReuse -gt $varHighest ]; then
-      let varHighest=varCountReuse
-    fi
+if [ -f "$1" ]; then
+  fileInput="$1"
+  checkForHashes=$(grep ".*:*:................................:................................" "$fileInput" | wc -l)
+  if [ "$checkForHashes" = "0" ]; then
+    echo
+    echo "Error: Expected format for NTLM password hashes/NTDS output not found."
+    echo "Checking with: 'grep .*:*:................................:................................'."
+    fnUsage
+    exit
   fi
-done < "$varTempUniqHashes"
-
-if [ ! -f "$varTempUnsortedCount" ]; then
+else
   echo
-  echo "Didn't find any hashes appearing more than once."
+  echo "Error: '$1' does not exist or is not a file."
+  fnUsage
+  exit
+fi
+
+# Read Options
+shift
+while [ "$1" != "" ]; do
+  case "$1" in
+  --out-dir )
+    shift
+    pathOut="$1"
+    if [ -e "$pathOut" ] && [ ! -d "$pathOut" ]; then
+      echo
+      echo "Error: '$pathOut' exists, but is not a directory."
+      fnUsage
+      exit
+    fi
+    ;;
+  --find-list )
+    findMode="LIST"
+    shift
+    fileFind="$1"
+    if [ ! -f "$fileFind" ]; then
+      echo
+      echo "Error: --find-list used, but '$fileFind' does not exist as a file."
+      fnUsage
+      exit
+    fi
+    ;;
+  --preserve-domain )
+    formatUsername="WITHDOMAIN"
+    ;;
+  * )
+    echo
+    echo "Error: Input not recognized."
+    fnUsage
+    exit
+    ;;
+  esac
+  shift
+done
+
+# Get reused NT hashes
+grep ".*:*:................................:................................" "$fileInput" | awk -F : '{print $4}' | sort | uniq -c | awk '{print $1 " x " $2}' | sort -Vr | grep -v '^1 x' > "$pathOut"/"$fileOutCount"
+
+# Check for reused-hashes-by-count output file before checking for users
+if [ -f "$pathOut"/"$fileOutCount" ]; then
+  # reused-hashes-by-count should be created, but empty, if no duplicates were found
+  # Check for its length before checking for users
+  countReusedHashes=$(wc -l "$pathOut"/"$fileOutCount" | awk '{print $1}')
+  if [ "$countReusedHashes" != "0" ]; then
+    echo
+    echo "Found $countReusedHashes NT password hashes reused."
+    mostReused=$(head -n 1 "$pathOut"/"$fileOutCount" | awk '{print $1}')
+    echo "The most-reused hash was reused $mostReused times."
+    echo
+    echo "$pathOut/$fileOutCount created."
+    # Check for corresponding users
+    while read -r "thisLine"; do
+      thisHash=$(echo "$thisLine" | awk '{print $3}')
+      thisUserList=""
+        grep ":................................:$thisHash" "$fileInput" | tr 'A-Z' 'a-z' > "$pathOut"/"$fileTemp"
+        while read -r "thisResult"; do
+          if [ "$formatUsername" = "NODOMAIN" ]; then
+            # Unless specified to preserve domains, check usernames for domain and drop it
+            checkForDomain=$(echo "$thisResult" | grep '\\')
+            if [ "$checkForDomain" != "" ]; then
+              thisUser=$(echo "$thisResult" | awk -F \\ '{print $2}' | awk -F: '{print $1}')
+            else
+              thisUser=$(echo "$thisResult" | awk -F: '{print $1}')
+            fi
+          else
+            thisUser=$(echo "$thisResult" | awk -F: '{print $1}')
+          fi
+          # Find users to highlight from a list
+          if [ "$findMode" = "LIST" ]; then
+            checkThisUser=$(grep -iw "$thisUser" "$fileFind" --color=always)
+            if [ "$checkThisUser" != "" ]; then
+              thisUser="$checkThisUser"
+            fi
+          fi
+          # Add user to rolling CSV list for this hash
+          thisUserList+=",$thisUser"
+        done < "$pathOut"/"$fileTemp"
+      # Begin output for file with users
+      echo "$thisLine" >> "$pathOut"/"$fileOutUsers"
+      echo "$thisUserList" | tr , "\n" | sort | tr "\n" , | sed 's/^,//g' | sed 's/,$//g' | sed 's/,/, /g' >> "$pathOut"/"$fileOutUsers"
+      echo >> "$pathOut"/"$fileOutUsers"
+      echo >> "$pathOut"/"$fileOutUsers"
+    done < "$pathOut"/"$fileOutCount"
+    # Cleanup and end
+    if [ -f "$pathOut"/"$fileTemp" ]; then rm "$pathOut"/"$fileTemp"; fi
+    if [ -f "$pathOut"/"$fileOutUsers" ]; then
+      echo
+      echo "$pathOut/$fileOutUsers created."
+      echo
+      echo "=======================================[ fin ]======================================="
+      echo
+      exit
+    fi
+  else
+    # If reused-hashes-by-count exists with 0 lines
+    echo
+    echo "Error: No reused NT hashes found, deleting '$fileOutCount'."
+    if [ -f "$pathOut"/"$fileOutCount" ]; then rm "$pathOut"/"$fileOutCount"; fi
+    echo
+    echo "=======================================[ fin ]======================================="
+    echo
+    exit
+  fi
+else
+  # If reused-hashes-by-count was not created
+  echo
+  echo "Error: No reused NT hashes found, '$fileOutCount' not created."
   echo
   echo "=======================================[ fin ]======================================="
   echo
-  if [ -f "$varTempAllHashes" ]; then rm "$varTempAllHashes"; fi
-  if [ -f "$varTempUniqHashes" ]; then rm "$varTempUniqHashes"; fi
   exit
-else
-  sort -nr "$varTempUnsortedCount" > "$varOutCount"
-  echo "Found $varCountTimes hashes reused a total of $varCountTotal times."
-  echo "The most reused password hash was found $varHighest times."
-  echo
-  echo "Reused hashes ordered by count saved as $varOutCount."
 fi
-
-# while read varLine; do varHash=$(echo "$varLine" | awk '{print $3}'); echo "$varLine"; grep $varHash combined-ntds.txt | awk -F: '{print $1}' | column; echo; done < reused-by-count.txt > reused-by-count-with-usernames.txt
-
-while read varLine; do
-  varThisHash=$(echo "$varLine" | awk '{print $3}')
-  echo "$varLine" >> "$varOutUsers"
-  grep "$varThisHash" "$varTempAllHashes" | awk -F: '{print $1}' | column >> "$varOutUsers"
-  echo >> "$varOutUsers"
-done < "$varOutCount"
-
-if [ -f "$varOutUsers" ]; then
-  echo
-  echo "Reused hashes ordered by count with users saved as $varOutUsers."
-fi
-
-if [ -f "$varTempAllHashes" ]; then rm "$varTempAllHashes"; fi
-if [ -f "$varTempUniqHashes" ]; then rm "$varTempUniqHashes"; fi
-if [ -f "$varTempUnsortedCount" ]; then rm "$varTempUnsortedCount"; fi
-echo
-echo "=======================================[ fin ]======================================="
-echo
